d5d36f808603c9053e082cc8a7558e302502057d
Akshay Ghodki <akshayghodki520@gmail.com>
2023-07-07 21:50:48 +0530

    TextUtills react website

--- src/App.js
+++ src/App.js
@@ -1,13 +1,65 @@
-import React from "react";
-   
-import UseState from "./component/Hooks/useState";
-import UseEffect from "./component/Hooks/useEffect";
-import UseReducer from "./component/Hooks/useReducer";
+import './App.css';
+import Navbar from './component/Navbar';
+import TextForm from './component/TextForm';
+// import About from './component/About';
+import React, { useState } from 'react';
+import Alert from './component/Alert';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
 
+ 
+function App() {
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
 
-const App = () => {
-  return <UseState />;
+  const showAlert = (message, type)=>{
+      setAlert({
+        msg: message,
+        type: type
+      })
+      setTimeout(() => {
+          setAlert(null);
+      }, 1500);
+  }
 
-};
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", "success");
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+    }
+  }
+  return (
+    <>
+    {/* <Router> */}
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
+    <Alert alert={alert}/>
+    <div className="container my-3">
+    <TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/>
+    {/* <Switch>
+    {/* /users --> Component 1
+        /users/home --> Component 2 */}
+          {/* <Route exact path="/about">
+            <About mode={mode} />
+          </Route>
+          <Route exact path="/">
+            <TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/>
+          </Route>
+    </Switch>
+    
+    </Router>  */}
+    </div>
+    </> 
+  );
+}
 
 export default App;
